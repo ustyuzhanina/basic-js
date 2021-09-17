02a05c816a61f9bc1051e9eb9275d6eefb66989f
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-09-17 16:22:25 +0700

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+    NotImplementedError
+} from '../extensions/index.js';
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,7 +19,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    let result = false;
+
+    if (+sampleActivity > 0) {
+        if (typeof sampleActivity === "string") {
+            result = Math.ceil(Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD));
+        }
+    }
+
+
+
+    return result > 0 ? result : result = false;
 }
