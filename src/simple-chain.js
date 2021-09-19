0dbd503240aeb04b7fd00a171598d11fbc51c7a8
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-09-19 22:38:21 +0700

    feat: add solution to simple-chain.js

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,28 +1,49 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+    NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Implement chainMaker object according to task description
  * 
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    buffer: [],
+
+    getLength() {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        return this.buffer.length;
+    },
+    addLink(value) {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        const ins = String(value) !== "undefined" ? value : "( )";
+        this.buffer.push(`( ${ins} )`);
+        return this;
+    },
+    removeLink(position) {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        if (typeof position !== "number" || position % 1 !== 0 || position < 1 || position > this.buffer.length) {
+            this.buffer = [];
+            throw new Error("You can\'t remove incorrect link!");
+        } else {
+            const copy = this.buffer.slice();
+            this.buffer = this.buffer.slice(0, position - 1).concat(copy.slice(position));
+            return this;
+        }
+    },
+    reverseChain() {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        this.buffer = this.buffer.reverse();
+        return this;
+    },
+    finishChain() {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        const result = this.buffer;
+        this.buffer = [];
+        return result.join("~~");
+    }
 };
