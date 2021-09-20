278bd83f984a0cb29278aef4dcf86daae031b19f
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-09-20 20:41:23 +0700

    feat: add solution to extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -17,7 +17,44 @@ import {
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater( /* str, options */ ) {
-    throw new NotImplementedError('Not implemented');
+export default function repeater(str, options) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let {
+        repeatTimes = 1,
+            separator = "+",
+            addition,
+            additionRepeatTimes = 1,
+            additionSeparator = "|",
+    } = options;
+
+    const string = typeof str === "string" ? str : String(str);
+
+    let additionString;
+    if (String(addition) !== "undefined") {
+        additionString = typeof addition === "string" ? addition : String(addition);
+    } else {
+        additionString = "";
+    }
+
+    const repetitionArr = [];
+
+    if (additionString) {
+        for (let i = 1; i <= additionRepeatTimes; i++) {
+            repetitionArr.push(additionString);
+        }
+    }
+
+    const insert = additionString ? repetitionArr.join(additionSeparator) : "";
+
+    const resArr = [];
+
+    for (let i = 1; i <= repeatTimes; i++) {
+        resArr.push(string + insert);
+    }
+
+    const result = resArr.join(separator);
+
+
+    return result;
 }
