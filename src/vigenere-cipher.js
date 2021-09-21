f7ae5463d42c2fef6e314a2849e7278a6b9d8fae
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-09-21 15:19:15 +0700

    init: start viginere-cipher.js task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+    NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
@@ -20,12 +22,42 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(direct) {
+        this.type = String(direct) === "false" ? "reverse" : "direct";
+        this.encrypt = this.encrypt.bind(this);
+        this.decrypt = this.decrypt.bind(this);
+        this.error = "Incorrect arguments!";
+        this.alphabet = [
+            65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
+        ];
+    }
+    encrypt(message, key) {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        if (!(message && key)) {
+            throw new Error(this.error);
+        }
+
+        let kf = Math.ceil(message.length / key.length);
+        key = key.repeat(kf);
+
+        const array = message.toUpperCase().split("").map(item => {
+            item.replace(/[A-Z]/, letter => {
+                let letterIdx = letter.charCodeAt(0) - this.alphabet[0];
+                let shift = key.charCodeAt(0) - this.alphabet[0];
+
+                return String.fromCharCode(this.alphabet[0] + (letterIdx + shift) % this.alphabet.length)
+            })
+        });
+
+        return this.type === "reverse" ? array.reverse().join("") : array.join("");
+    }
+
+    decrypt(encryptedMessage, key) {
+        // throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+        if (!(message && key)) {
+            throw new Error(this.error);
+        }
+    }
 }
