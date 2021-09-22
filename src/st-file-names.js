b70a2039c942e2f0cb2aa577b40bd3afee18b2ac
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-09-22 18:24:37 +0700

    feat: add solution to st-file-names.js

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+    NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,7 +17,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    //create object with files and counts
+    const library = {};
+
+    //go through an array of files and check whether the obj above has such name, add uniques, change the name of existing
+    return names.map(file => {
+
+        let fileName;
+        if (file in library) {
+            library[file] = library[file] + 1;
+            fileName = `${file}(${library[file]})`
+            library[fileName] = 0;
+        } else {
+            library[file] = 0;
+            fileName = file;
+        }
+
+        return fileName;
+    })
+
 }
