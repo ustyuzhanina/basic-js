24b1f1f485b196a9ff2a387980a5934a50db26ac
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-09-23 12:49:54 +0700

    feat: add solution to st-sum-digits.js

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+    NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Given a string, return its encoding version.
@@ -8,9 +10,44 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * @example
  * For aabbbc should return 2a3bc
- *
+ * encodeLine("aabbbc")
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    const strArray = str.split('');
+    //strArray.push('!');
+    let letterCount;
+    const result = [];
+
+
+
+    for (let i = 0; i <= strArray.length; i++) {
+        const previous = strArray[i - 1];
+        const current = strArray[i];
+
+        if (current === previous) {
+            //console.log(current);
+            letterCount[current] = letterCount[current] + 1;
+            //console.log(letterCount)
+
+        } else {
+            console.log(String(current));
+
+            if (String(current) !== "undefined") {
+                letterCount[current] = 1;
+                console.log(letterCount);
+                const insert = letterCount[previous] === 1 ? previous : letterCount[previous] + previous;
+                //console.log(insert);
+                result.push(insert);
+                letterCount = {};
+            }
+
+        }
+
+    }
+
+    return result.join('');
+
 }
--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+    NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Given a number, replace this number with
@@ -12,7 +14,13 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    if (String(n).length > 1) {
+        return getSumOfDigits(String(n).split('').map(str => Number(str)).reduce((cum, val) => cum + val));
+    } else {
+        return n;
+    }
 }
